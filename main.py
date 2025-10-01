7579b773fe3c15f30464b19573cbc5a80cc7e027
joske920 <jimmybamps@hotmail.com>
2025-10-01 09:47:18 -0400

    added dictionary sort

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_num_words, get_character_count
+from stats import get_num_words, get_character_count, sort_dict
 
 def get_book_text(filepath:str) -> str:
     """
@@ -9,9 +9,17 @@ def get_book_text(filepath:str) -> str:
     return filecontents
 
 def main():
-    text = get_book_text(r"books/frankenstein.txt")
+    filepath = r"books/frankenstein.txt"
+    text = get_book_text(filepath)
     num_words = get_num_words(text)
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {filepath}...")
+    print("----------- Word Count ----------")
     print(f"Found {num_words} total words")
-    print(get_character_count(text))
+    print("--------- Character Count -------")
+    for item in sort_dict(get_character_count(text)):
+        if item["char"].isalpha():
+            print(f"{item["char"]}: {item["num"]}")
+    print("============= END ===============")
 
 main()
--- stats.py
+++ stats.py
@@ -12,3 +12,20 @@ def get_character_count(text:str) -> dict:
             character_dict[c] = 1
 
     return character_dict
+
+def sort_dict(char_dict:dict) -> list:
+    """
+    Takes a dictionary of characters and their counts,
+    returns a sorted list of dictionaries in descending order of counts.
+    """
+    # Convert {char: count} into [{"char": char, "num": count}, ...]
+    char_list = [{"char": c, "num": n} for c, n in char_dict.items()]
+    
+    # Helper function for sorting
+    def get_num(entry):
+        return entry["num"]
+    
+    # Sort in-place, from greatest to least
+    char_list.sort(key=get_num, reverse=True)
+    
+    return char_list
